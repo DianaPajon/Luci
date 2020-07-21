3136aaef2b8857358963c6451a9304de55ed879d
Diana Pajón <diana.pajon.16@gmail.com>
2020-07-21 13:54:10 -0300

    Agregada la clase precondition

---
+++ src/main/java/com/aura/luci/chatbot/luciml/Precondition.java
@@ -0,0 +1,28 @@
+package com.aura.luci.chatbot.luciml;
+
+public class Precondition {
+	private String variable;
+	private String value;
+	
+	public Precondition(String variable, String value) {
+		
+	}
+
+	public String getVariable() {
+		return variable;
+	}
+
+	public void setVariable(String variable) {
+		this.variable = variable;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	
+}
